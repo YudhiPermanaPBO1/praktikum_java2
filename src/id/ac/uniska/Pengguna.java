ddf2f0a1108feb61626977083d12bd2560c96aa0
YudhiPermanaPBO1 <yudhi.gotan.10@gmail.com>
2022-06-05 11:26:17 +0800

    Membuat constructor pengguna

--- src/id/ac/uniska/Pengguna.java
+++ src/id/ac/uniska/Pengguna.java
@@ -6,4 +6,15 @@ public class Pengguna {
     private String password;
     private String namaLengkap;
     private String level;
+
+    public Pengguna() {
+    }
+
+    public Pengguna(int id, String username, String password, String namaLengkap, String level) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.namaLengkap = namaLengkap;
+        this.level = level;
+    }
 }
